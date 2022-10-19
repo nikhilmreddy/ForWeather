f54f914b928e10f70f14ffc21eebb50be6a5ee41
nikhilmreddy <nikhil@ikshalabs.com>
2022-10-19 11:26:55 +0530

    added option to search and display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import './App.css';
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Navbar from "./components/Navbar";
 import Weathercard from "./components/Weathercard"
-import data from "./data";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  console.log(data)
-  const weatherElements=data.map((item)=> {
-    return (
-      <Weathercard 
-        key={item.id}
-        {...item} 
-      />
-    )
-  })
+//create a state to store place which i got from getPlace
+  const [cityData,setCityData]=useState({})
+  const [place,setPlace]=useState("")
+
+  useEffect(()=> {
+      async function fetchApi() {
+      const url=`https://api.openweathermap.org/data/2.5/weather?q=${place}%20&APPID=17451272b5e4b828fefb89a8e215e3db`
+      const res= await fetch(url)
+      const weatherData= await res.json()
+      setCityData(weatherData)
+    }
+    fetchApi();
+  },[place])
+
+  function getPlace(inputPlace) {
+    setPlace(inputPlace)
+  }
   return (
     <div>
-      <Navbar />
-      {weatherElements}
+      <Navbar change={getPlace}/>
+      {cityData.main && <Weathercard {...cityData}/>}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 import pic from "./logo.jpg";
 import Form from "react-bootstrap/Form";
 import "./Navbar.css";
 import Button from 'react-bootstrap/Button';
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const [userPlace,setUserPlace]=useState("")
+  
   return (
     <nav className="navbar">
       <img src={pic} alt="forWeather logo" className="image"></img>
@@ -15,8 +17,13 @@ export default function Navbar() {
           className="text-area"
           placeholder="Eg. Delhi"
           rows={1}
+          onChange={(e)=>{
+            setUserPlace(e.target.value)
+          }}
+          value={userPlace}
+          
         />
-        <Button variant="primary">&#8680;</Button>{' '}
+        <Button onClick={()=>props.change(userPlace)} variant="primary">&#8680;</Button>{' '}
       </div>
     </nav>
   );
